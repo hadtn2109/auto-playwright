214e5bf9ffe0acacdabe595a84b43f472dbda685
hadtn2109 <dtngocha2109@gmail.com>
2025-03-29 08:29:56 +0700

    delete file

--- tests/gencode1.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from "@playwright/test";
-test("test", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  await page.getByRole("link", { name: "Get started" }).click();
-  await page.getByRole("link", { name: "Test configuration" }).click();
-});
